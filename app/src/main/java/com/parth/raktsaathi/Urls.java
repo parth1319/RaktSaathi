90f6c81fd9880004ae204b6686b5e343bee412bd
Parth Pande <pandeparth038@gmail.com>
2026-03-20 03:15:01 +0530

    Button Created On HomeScreen

--- app/src/main/java/com/parth/raktsaathi/Urls.java
+++ app/src/main/java/com/parth/raktsaathi/Urls.java
@@ -2,13 +2,9 @@ package com.parth.raktsaathi;
 
 public class Urls {
 
-    public static final String RegisterUserWebServiceAddress = "http://10.171.187.206:80/RaktsaathiAPI/registerUser.php";
-    public static final String LoginUserWebService = "http://10.171.187.206:80/RaktsaathiAPI/loginUsers.php";
-    public static final String ForgetPasswordWebService ="http://10.171.187.206:80/RaktsaathiAPI/forgetpassword.php";
-    public static final String GetDonorsWebService ="http://10.171.187.206:80/RaktsaathiAPI/get_donars.php";
-    public static final String GetCityWebService = "http://10.171.187.206:80/RaktsaathiAPI/getCity.php";
-
-    public static final String DonateBloodWebService = "http://10.209.67.98:80/RaktsaathiAPI/donate_blood_details.php";
-    public static final String RequestBloodWebService = "http://10.209.67.98:80/RaktsaathiAPI/request_blood_details.php";
-
+    public static final String RegisterUserWebServiceAddress = "http://10.171.187.98:80/RaktsaathiAPI/registerUser.php";
+    public static final String LoginUserWebService = "http://10.171.187.98:80/RaktsaathiAPI/loginUsers.php";
+    public static final String ForgetPasswordWebService ="http://10.171.187.98:80/RaktsaathiAPI/forgetpassword.php";
+    public static final String GetDonorsWebService ="http://10.171.187.98:80/RaktsaathiAPI/get_donars.php";
+    public static final String GetCityWebService = "http://10.171.187.98:80/RaktsaathiAPI/getCity.php";
 }
