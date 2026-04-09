b252036a0a74cf4b7b9b8b005cafe3f91678b35c
soniya tandulkar <soniyatandulkar50@gmail.com>
2026-04-10 02:32:07 +0530

    modified HealthTipsActivity

--- app/src/main/java/com/parth/raktsaathi/HealthTipsActivity.java
+++ app/src/main/java/com/parth/raktsaathi/HealthTipsActivity.java
@@ -2,44 +2,90 @@ package com.parth.raktsaathi;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HealthTipsActivity extends AppCompatActivity {
 
-    TextView tvLong1, tvLong2, tvLong3, tvLong4;
-    TextView btnMore1, btnMore2, btnMore3, btnMore4;
+    TextView tv1, tv2, tv3, tv4;
+    Button btn1, btn2, btn3, btn4;
+
+    boolean isExpanded1 = false;
+    boolean isExpanded2 = false;
+    boolean isExpanded3 = false;
+    boolean isExpanded4 = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_health_tips);
 
-        tvLong1 = findViewById(R.id.tv1);
-        tvLong2 = findViewById(R.id.tv2);
-        tvLong3 = findViewById(R.id.tv3);
-        tvLong4 = findViewById(R.id.tv4);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv3 = findViewById(R.id.tv3);
+        tv4 = findViewById(R.id.tv4);
 
-        btnMore1 = findViewById(R.id.btn1);
-        btnMore2 = findViewById(R.id.btn2);
-        btnMore3 = findViewById(R.id.btn3);
-        btnMore4 = findViewById(R.id.btn4);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
 
-        btnMore1.setOnClickListener(v -> toggle(tvLong1, btnMore1));
-        btnMore2.setOnClickListener(v -> toggle(tvLong2, btnMore2));
-        btnMore3.setOnClickListener(v -> toggle(tvLong3, btnMore3));
-        btnMore4.setOnClickListener(v -> toggle(tvLong4, btnMore4));
+        btn1.setOnClickListener(v -> toggleText(tv1, btn1, 1));
+
+        btn2.setOnClickListener(v -> toggleText(tv2, btn2, 2));
+
+        btn3.setOnClickListener(v -> toggleText(tv3, btn3, 3));
+
+        btn4.setOnClickListener(v -> toggleText(tv4, btn4, 4));
     }
 
-    private void toggle(TextView longText, TextView button) {
+    private void toggleText(TextView tv, Button btn, int index) {
 
-        if (longText.getVisibility() == View.GONE) {
-            longText.setVisibility(View.VISIBLE);
-            button.setText("See Less");
-        } else {
-            longText.setVisibility(View.GONE);
-            button.setText("See More");
+        switch (index) {
+            case 1:
+                isExpanded1 = !isExpanded1;
+                if (isExpanded1) {
+                    tv.setMaxLines(Integer.MAX_VALUE);
+                    btn.setText("See Less");
+                } else {
+                    tv.setMaxLines(2);
+                    btn.setText("See More");
+                }
+                break;
+
+            case 2:
+                isExpanded2 = !isExpanded2;
+                if (isExpanded2) {
+                    tv.setMaxLines(Integer.MAX_VALUE);
+                    btn.setText("See Less");
+                } else {
+                    tv.setMaxLines(2);
+                    btn.setText("See More");
+                }
+                break;
+
+            case 3:
+                isExpanded3 = !isExpanded3;
+                if (isExpanded3) {
+                    tv.setMaxLines(Integer.MAX_VALUE);
+                    btn.setText("See Less");
+                } else {
+                    tv.setMaxLines(2);
+                    btn.setText("See More");
+                }
+                break;
+
+            case 4:
+                isExpanded4 = !isExpanded4;
+                if (isExpanded4) {
+                    tv.setMaxLines(Integer.MAX_VALUE);
+                    btn.setText("See Less");
+                } else {
+                    tv.setMaxLines(2);
+                    btn.setText("See More");
+                }
+                break;
         }
     }
 }
